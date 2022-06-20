cabf5845d27f899e1a3b2e88f86a45b0b0ec61a2
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 15:53:36 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 
+let el = document.body;
+let str = "こんにちは！";
 
-count = 0; //カウントの初期値
-timerID = setInterval(&#39;countup()&#39;,1000); //1秒毎にcountup()を呼び出し
-
-function countup() {
-	count++;
-	document.form_count.counter.value = count;
-}
-
-
+  
+el.textContent = str;
