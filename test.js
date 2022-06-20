8989a28d1f6c8a5313f0a471fe2d5aff2d77f42d
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 16:45:31 +0900

    Update test.js

--- test.js
+++ test.js
@@ -12,3 +12,27 @@ function countup() {
     var doc0= document.getElementById("div0");  
     doc0.innerHTML= "gibhutに javaScript を格納！";   
 
+;(function(d){
+if(!window.getSelection){
+  return
+}
+var btn = d.createElement(&quot;button&quot;)
+btn.id = &quot;selectPre&quot;
+btn.textContent = &quot;ソースコード全選択&quot;
+btn.addEventListener(&quot;click&quot;, selectPre, false)
+function selectPre(){
+  var sel = window.getSelection()
+  var pre = this.parentNode
+  sel.selectAllChildren(pre)
+  sel.extend(pre, pre.childNodes.length-1)
+}
+var pres = d.getElementsByTagName(&quot;code&quot;)
+for(var i=pres.length; i--;){
+  pres[i].addEventListener(&quot;mouseover&quot;, addBtn, false)
+}
+function addBtn(e){
+  if(this === addBtn.ele) return // not to addBtn if already
+  this.appendChild(btn)
+  return addBtn.ele = this
+}
+})(document)
