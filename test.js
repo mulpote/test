4e30d100937b97ecdd17eeca8729a77f7fd4e1c3
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 15:29:04 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-
-<script type='text/javascript'>
-<!--
 count = 0; //カウントの初期値
-timerID = setInterval('countup()',1000); //1秒毎にcountup()を呼び出し
+timerID = setInterval(&#39;countup()&#39;,1000); //1秒毎にcountup()を呼び出し
 
 function countup() {
 	count++;
 	document.form_count.counter.value = count;
 }
--->
-</script>
