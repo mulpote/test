24cfc44c2f55807bf983a6f7cfe26364f9b9c924
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 14:40:46 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,23 @@
-function(d){
+
+<!-- highlight.js ボタン -->
+<script defer>
+;(function(d){
 if(!window.getSelection){
   return
 }
-var btn = d.createElement(&quot;button&quot;)
-btn.id = &quot;selectPre&quot;
-btn.textContent = &quot;ソースコード全選択&quot;
-btn.addEventListener(&quot;click&quot;, selectPre, false)
+var btn = d.createElement("button")
+btn.id = "selectPre"
+btn.textContent = "ソースコード全選択"
+btn.addEventListener("click", selectPre, false)
 function selectPre(){
   var sel = window.getSelection()
   var pre = this.parentNode
   sel.selectAllChildren(pre)
   sel.extend(pre, pre.childNodes.length-1)
 }
-var pres = d.getElementsByTagName(&quot;code&quot;)
+var pres = d.getElementsByTagName("code")
 for(var i=pres.length; i--;){
-  pres[i].addEventListener(&quot;mouseover&quot;, addBtn, false)
+  pres[i].addEventListener("mouseover", addBtn, false)
 }
 function addBtn(e){
   if(this === addBtn.ele) return // not to addBtn if already
@@ -22,3 +25,4 @@ function addBtn(e){
   return addBtn.ele = this
 }
 })(document)
+</script>
