14eb36f12856519f341a0b02c86ac1bf713969c4
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 16:21:49 +0900

    Update test.js

--- test.js
+++ test.js
@@ -8,29 +8,7 @@ function countup() {
 }
 -->
 
-<!--
-function(d){
-if(!window.getSelection){
-  return
-}
-var btn = d.createElement("button")
-btn.id = "selectPre"
-btn.textContent = "ソースコード全選択"
-btn.addEventListener("click", selectPre, false)
-function selectPre(){
-  var sel = window.getSelection()
-  var pre = this.parentNode
-  sel.selectAllChildren(pre)
-  sel.extend(pre, pre.childNodes.length-1)
-}
-var pres = d.getElementsByTagName("code")
-for(var i=pres.length; i--;){
-  pres[i].addEventListener("mouseover", addBtn, false)
-}
-function addBtn(e){
-  if(this === addBtn.ele) return // not to addBtn if already
-  this.appendChild(btn)
-  return addBtn.ele = this
-}
-}
--->
+
+    var doc0= document.getElementById("div0");  
+    doc0.innerHTML= "div0の箇所にテキストを表示します。";   
+
