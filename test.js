4b3bb21a814a6e39f2cc35f7a5500c1725b89541
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 14:12:55 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,27 @@
+<!-- highlight.js ボタン -->
+<script defer='defer'>
+;(function(d){
+if(!window.getSelection){
+  return
+}
+var btn = d.createElement(&quot;button&quot;)
+btn.id = &quot;selectPre&quot;
+btn.textContent = &quot;ソースコード全選択&quot;
+btn.addEventListener(&quot;click&quot;, selectPre, false)
+function selectPre(){
+  var sel = window.getSelection()
+  var pre = this.parentNode
+  sel.selectAllChildren(pre)
+  sel.extend(pre, pre.childNodes.length-1)
+}
+var pres = d.getElementsByTagName(&quot;code&quot;)
+for(var i=pres.length; i--;){
+  pres[i].addEventListener(&quot;mouseover&quot;, addBtn, false)
+}
+function addBtn(e){
+  if(this === addBtn.ele) return // not to addBtn if already
+  this.appendChild(btn)
+  return addBtn.ele = this
+}
+})(document)
+</script>
