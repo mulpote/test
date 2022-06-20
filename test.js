67f5192edfec5f662556c7264576d3cbd16f8eb6
mulpote <107822665+mulpote@users.noreply.github.com>
2022-06-20 16:15:58 +0900

    Update test.js

--- test.js
+++ test.js
@@ -7,3 +7,29 @@ function countup() {
 	document.form_count.counter.value = count;
 }
 -->
+
+
+function(d){
+if(!window.getSelection){
+  return
+}
+var btn = d.createElement("button")
+btn.id = "selectPre"
+btn.textContent = "ソースコード全選択"
+btn.addEventListener("click", selectPre, false)
+function selectPre(){
+  var sel = window.getSelection()
+  var pre = this.parentNode
+  sel.selectAllChildren(pre)
+  sel.extend(pre, pre.childNodes.length-1)
+}
+var pres = d.getElementsByTagName("code")
+for(var i=pres.length; i--;){
+  pres[i].addEventListener("mouseover", addBtn, false)
+}
+function addBtn(e){
+  if(this === addBtn.ele) return // not to addBtn if already
+  this.appendChild(btn)
+  return addBtn.ele = this
+}
+}
